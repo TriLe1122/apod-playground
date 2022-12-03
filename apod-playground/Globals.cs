f9056c1a1133458dcf0a931b83883165a2f8154a
tri le <letrin93@gmail.com>
2022-12-02 22:00:41 -0700

    project initialized

---
+++ apod-playground/Globals.cs
@@ -0,0 +1,16 @@
+global using System;
+global using System.Data;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Threading.Tasks;
+global using Dapper;
+global using CodeWorks.Utils;
+
+// APPLICATION SPECIFIC
+global using apod-playground.Repositories;
+global using apod-playground.Services;
+global using apod-playground.Models;
